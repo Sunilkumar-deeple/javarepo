845096dc8963bfdb782594c1b94368ed10201b43
sunilkumar-deeple <sunilkumardeeple2022@gmail.com>
2024-02-07 01:22:48 +0530

    here addition done

--- src/Addition.java
+++ src/Addition.java
@@ -1,5 +1,12 @@
 public class Addition {
     public static void main(String[] args) {
         System.out.println(20+30);
+
+
+        //addition
+        int a=30;
+        int b=30;
+        int c=a+b;
+        System.out.println(c);
     }
 }
